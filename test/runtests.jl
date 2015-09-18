b0b609e98965e804a478f006951a0ec0b5def8c7
generativist <jbn@abreka.com>
2015-09-18 18:13:51 -0400

    Use FactCheck instead of Base.Test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,41 +1,56 @@
 using OAuth
 using Compat
-using Base.Test
+using FactCheck
 
 
+facts("oauth_timestamp") do
+    context("returns a string") do
+        @fact typeof(oauth_timestamp()) <: String --> true
+    end
 
-#Test that timestamp comes out as a string
-@test typeof(oauth_timestamp()) <: String
+    context("returns a value representing a time after 2014-01-25 20:25:00") do
+        @compat @fact parse(Int, oauth_timestamp()) > 1422235471 --> true
+    end
+end
 
-#Test that timestamp is greater than integer representing approx. 2014-01-25 20:25:00
-@test @compat parse(Int, oauth_timestamp()) > 1422235471
+facts("ouath_nonce") do
+    context("returns a string") do 
+        @fact typeof(oauth_nonce(15)) <: String --> true
+    end
 
+    context("with a length equal to the parameter length") do 
+        @fact length(oauth_nonce(15)) --> 15
+        @fact length(oauth_nonce(20)) --> 20
+        @fact length(oauth_nonce(25)) --> 25
+        @fact length(oauth_nonce(30)) --> 30
+        @fact length(oauth_nonce(32)) --> 32
+    end
+end
 
+facts("oauth_sign_hmac_sha1") do
+    context("provides a consistent string") do 
+        expected = "WqKCG5iyhFiES3fWYVdWJWwinaY="
+        @fact oauth_sign_hmac_sha1("randy", "zwitch") --> expected
+    end
+end
 
-#Test that nonce comes out as a string
-@test typeof(oauth_nonce(15)) <: String
+facts("oauth_signing_key") do 
+    context("returns a concatenation of values, seperated by &") do
+        result = oauth_signing_key("9djdj82h48djs9d2", "kkk9d7dh3k39sjv7")
+        expected = "9djdj82h48djs9d2&kkk9d7dh3k39sjv7"
+        @fact result --> expected
+    end
+end
 
-#Test that nonce comes out same length as passed argument
-@test length(oauth_nonce(15)) == 15
-@test length(oauth_nonce(20)) == 20
-@test length(oauth_nonce(25)) == 25
-@test length(oauth_nonce(30)) == 30
-@test length(oauth_nonce(32)) == 32
-
-
-
-#Test that HMAC-SHA1 signature provides consistent string
-@test oauth_sign_hmac_sha1("randy","zwitch") == "WqKCG5iyhFiES3fWYVdWJWwinaY="
-
-
-
-#Validate that signing key a concatenation of values, separated by &
-@test oauth_signing_key("9djdj82h48djs9d2", "kkk9d7dh3k39sjv7") == "9djdj82h48djs9d2&kkk9d7dh3k39sjv7"
-
-
-
-#Test that base string is concatenated then percent-encoded
-@test oauth_signature_base_string("POST", "http://example.com", "?julia=fast&lang=elegant") == "POST&http%3A%2F%2Fexample.com&%3Fjulia%3Dfast%26lang%3Delegant"
+facts("oauth_signature_base_string") do 
+    context("returns a concatinated and percent-encoded string") do 
+        result = oauth_signature_base_string(
+            "POST", "http://example.com", "?julia=fast&lang=elegant"
+        )
+        expected = "POST&http%3A%2F%2Fexample.com&%3Fjulia%3Dfast%26lang%3Delegant"
+        @fact result --> expected
+    end
+end
 
 
 
