f17bfbc2ccf1c302797509b32693075591607400
randyzwitch <rzwitch@gmail.com>
2017-12-03 19:50:26 -0500

    Fix missing comma

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ makedocs()
 
 deploydocs(
     deps = Deps.pip("mkdocs", "python-markdown-math"),
-    repo = "github.com/randyzwitch/OAuth.jl.git"
+    repo = "github.com/randyzwitch/OAuth.jl.git",
     julia  = "0.6",
     osname = "linux"
 )
