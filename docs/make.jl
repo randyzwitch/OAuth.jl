708b7c693fc3e2d61ac13475753effc672b95efe
randyzwitch <rzwitch@gmail.com>
2017-12-04 21:49:23 -0500

    Update mkdocs

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, OAuth
 makedocs()
 
 deploydocs(
-    deps = Deps.pip("mkdocs", "python-markdown-math", "mkdocs-bootswatch"),
+    deps = Deps.pip("mkdocs==0.16.3", "python-markdown-math", "mkdocs-bootswatch"),
     repo = "github.com/randyzwitch/OAuth.jl.git",
     julia  = "0.6",
     osname = "linux"
