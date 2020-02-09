05c8005bf8d7736f319b105ae8f6f8cf57c5e247
Randy Zwitch <randy.zwitch@omnisci.com>
2020-02-09 14:52:45 -0500

    Switch to project.toml (#27)

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,12 @@
 using Documenter, OAuth
 
-makedocs()
+makedocs(
+    sitename = "OAuth.jl"
+)
 
 deploydocs(
     deps = Deps.pip("mkdocs==0.16.3", "python-markdown-math", "mkdocs-bootswatch"),
     repo = "github.com/randyzwitch/OAuth.jl.git",
-    julia  = "0.6",
+    julia  = "1.3",
     osname = "linux"
 )
